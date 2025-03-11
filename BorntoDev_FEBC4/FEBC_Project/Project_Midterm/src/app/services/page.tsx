6099a56da38f0589f67ff0d9ea33a69247df3619
Theetawat Premsawat <theetawat.psw@gmail.com>
2025-03-11 20:27:14 +0700

    commit

---
+++ BorntoDev_FEBC4/FEBC_Project/Project_Midterm/src/app/services/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ServicesSection } from '../components/sections/ServicesSection';
+import { TestimonialsSection } from '../components/sections/TestimonialsSection';
+
+const Services = () => {
+    return (
+        <div>
+            <ServicesSection />
+            <TestimonialsSection />
+        </div>
+    );
+};
+
+export default Services;
